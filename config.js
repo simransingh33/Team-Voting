88405de42e0d2eac9facce95f0d15aad5c9e9d79
simransingh33 <simransandhu7766@gmail.com>
2021-12-22 15:23:38 +0530

    type here

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyCb89nHT6sYJifhDa2aTsGfDFCbH7WiSnM",
+    authDomain: "c-67-599b9.firebaseapp.com",
+    databaseURL: "https://c-67-599b9-default-rtdb.firebaseio.com",
+    projectId: "c-67-599b9",
+    storageBucket: "c-67-599b9.appspot.com",
+    messagingSenderId: "979182835456",
+    appId: "1:979182835456:web:cc0f265463a479f5b585e5"
+  };
 
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
